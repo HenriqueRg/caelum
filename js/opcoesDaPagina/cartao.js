0d9a23292d149f42545300212845fc57f1cb6caa
HenriqueRg <web7760@caelum131-08>
2018-11-13 22:20:55 -0200

    aula02 ex13

--- js/opcoesDaPagina/cartao.js
+++ js/opcoesDaPagina/cartao.js
@@ -1,12 +1,15 @@
 ;(function(){
-    const btn = document.querySelector('.opcoesDoCartao-remove')
+    const btns = document.querySelectorAll('.opcoesDoCartao-remove')
+    
 
-    function removeCartao(){
-        const cartao = btn.parentNode.parentNode
-        cartao.classList.add('cartao--some')
-        cartao.addEventListener("transitionend",function(){
-            cartao.remove()
+    for(let i=0 ; i < btns.length; i++){
+        btns[i].addEventListener('click', function(){
+            const cartao = btns[i].parentNode.parentNode
+            cartao.classList.add("cartao--some")
+            cartao.addEventListener('transitionend', function(){
+                cartao.remove()
+            })
         })
     }
-    btn.onclick = removeCartao
+    
 })()
