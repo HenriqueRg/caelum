4b392e0c835e113f6d5b0b022fa6a0a889caac9d
HenriqueRg <web7760@caelum131-08>
2018-11-22 19:06:15 -0200

    ultimo curso

--- js/opcoesDaPagina/cartao.js
+++ js/opcoesDaPagina/cartao.js
@@ -1,21 +1,3 @@
-;(function(){
-    const btns = document.querySelectorAll('.opcoesDoCartao-remove')
-    
-
-    for(let i=0 ; i < btns.length; i++){
-        btns[i].addEventListener('click', function(){
-            const cartao = btns[i].parentNode.parentNode
-            cartao.classList.add("cartao--some")
-            cartao.addEventListener('transitionend', function(){
-                cartao.remove()
-            })
-        })
-        
-    }
-    const cartoes = document.querySelectorAll(".cartao")
-
-    for(let j = 0; j < cartoes.length; j++){
-        const cartao = cartoes[j]
 
         cartao.addEventListener("change", function mudaCor(event){
             const elementoSelecionado = event.target
